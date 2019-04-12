fc26e92e94e26733fefcdb84d2d9f6ff4da7ffd1
SCloves <clovesgtx@gmail.com>
2019-04-12 18:59:57 -0300

    ainda lidando com pot-pourri

--- save2neo.py
+++ save2neo.py
@@ -133,28 +133,28 @@ def CreateFaixas(faixas, album):
         for musica in faixa_album['musica']:
             musica = trataString(musica)
             nome_album = trataString(album['album'])
-            if musica != 'Pot-pourri': # por enquanto estamos ignorando
+            if musica.lower() == 'pot-pourri': # por enquanto estamos ignorando
                 break
-            else:
-                CreateMusica(musica)
-                CreateRelacaoMusicaAlbum(musica, nome_album, item)
-                compositores = faixa_album.get('compositores')
-                if compositores:
-                    CreateRelacaoCompositoresMusica(compositores, musica)
-                musicos = faixa_album.get('musicos')
-                if musicos:
-                    CreateMusicos(musicos, musica, nome_album, item)
-                arranjadores = faixa_album.get('arranjadores')
-                if arranjadores:
-                    CreateRelacaoArranjadoresMusica(musica,
+
+            CreateMusica(musica)
+            CreateRelacaoMusicaAlbum(musica, nome_album, item)
+            compositores = faixa_album.get('compositores')
+            if compositores:
+                CreateRelacaoCompositoresMusica(compositores, musica)
+            musicos = faixa_album.get('musicos')
+            if musicos:
+                CreateMusicos(musicos, musica, nome_album, item)
+            arranjadores = faixa_album.get('arranjadores')
+            if arranjadores:
+                CreateRelacaoArranjadoresMusica(musica,
                                                 arranjadores,
                                                 nome_album, item)
-                    CreateRelacaoArranjadoresAlbum(musica,
+                CreateRelacaoArranjadoresAlbum(musica,
                                              arranjadores,
                                              nome_album, item)
-                participantes_especiais = faixa_album.get('participacaoEspecial')
-                if participantes_especiais:
-                    CreateRelacaoParticipanteMusica(participantes_especiais,
+            participantes_especiais = faixa_album.get('participacaoEspecial')
+            if participantes_especiais:
+                CreateRelacaoParticipanteMusica(participantes_especiais,
                                                 musica,
                                                 nome_album)
 
